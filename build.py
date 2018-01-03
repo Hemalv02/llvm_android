aosp-riscv/toolchain_llvm_android

Fix host crt build
Upstream change https://reviews.llvm.org/D26764 removed i686 target of
crt in favour of i386. Change host crt build to i386.

Also changed SANITIZER_CXX_ABI to libstdc++.

Change-Id: I1a8fe20cbb84733a42bf882201e347e11dea3f54

build.py
@@ -529,7 +529,7 @@ def build_libomp(stage2_install, clang_version, ndk_cxx=False):
         shutil.copy2(static_lib, os.path.join(lib_dir, 'libomp.a'))
 
 
-def build_crts_host_i686(stage2_install, clang_version):
+def build_crts_host_i386(stage2_install, clang_version):
     llvm_config = os.path.join(stage2_install, 'bin', 'llvm-config')
 
     crt_install = os.path.join(stage2_install, 'lib64', 'clang',
@@ -546,11 +546,11 @@ def build_crts_host_i686(stage2_install, clang_version):
     crt_defines['COMPILER_RT_DEFAULT_TARGET_ONLY'] = 'ON'
 
     # Due to CMake and Clang oddities, we need to explicitly set
-    # CMAKE_C_COMPILER_TARGET and use march=i686 in cflags below instead of
+    # CMAKE_C_COMPILER_TARGET and use march=i386 in cflags below instead of
     # relying on auto-detection from the Compiler-rt CMake files.
-    crt_defines['CMAKE_C_COMPILER_TARGET'] = 'i686-linux-gnu'
+    crt_defines['CMAKE_C_COMPILER_TARGET'] = 'i386-linux-gnu'
 
-    cflags = ['--target=i686-linux-gnu', "-march=i686"]
+    cflags = ['--target=i386-linux-gnu', "-march=i386"]
     crt_defines['CMAKE_C_FLAGS'] = ' '.join(cflags)
     crt_defines['CMAKE_CXX_FLAGS'] = ' '.join(cflags)
 
@@ -558,11 +558,13 @@ def build_crts_host_i686(stage2_install, clang_version):
     crt_defines['COMPILER_RT_INCLUDE_TESTS'] = 'ON'
     crt_defines['COMPILER_RT_ENABLE_WERROR'] = 'ON'
     crt_defines['CMAKE_INSTALL_PREFIX'] = crt_install
-    crt_defines['SANITIZER_CXX_ABI'] = 'libc++'
+    crt_defines['SANITIZER_CXX_ABI'] = 'libstdc++'
+
+    crt_defines['COMPILER_RT_BUILD_LIBFUZZER'] = 'OFF'
 
     crt_env = dict(ORIG_ENV)
 
-    crt_path = utils.out_path('lib', 'clangrt-i686-host')
+    crt_path = utils.out_path('lib', 'clangrt-i386-host')
     rm_cmake_cache(crt_path)
     invoke_cmake(
         out_path=crt_path,
@@ -818,7 +820,7 @@ def build_stage2(stage1_install,
 def build_runtimes(stage2_install):
     version = extract_clang_version(stage2_install)
     build_crts(stage2_install, version)
-    build_crts_host_i686(stage2_install, version)
+    build_crts_host_i386(stage2_install, version)
     build_libfuzzers(stage2_install, version)
     build_libfuzzers(stage2_install, version, ndk_cxx=True)
     build_libomp(stage2_install, version)

Hemalv02/llvm_android

build.py
@@ -548,9 +548,9 @@ def build_crts_host_i686(stage2_install, clang_version):
     # Due to CMake and Clang oddities, we need to explicitly set
     # CMAKE_C_COMPILER_TARGET and use march=i686 in cflags below instead of
     # relying on auto-detection from the Compiler-rt CMake files.
-    crt_defines['CMAKE_C_COMPILER_TARGET'] = 'i686-linux-gnu'
+    crt_defines['CMAKE_C_COMPILER_TARGET'] = 'i386-linux-gnu'
 
-    cflags = ['--target=i686-linux-gnu', "-march=i686"]
+    cflags = ['--target=i386-linux-gnu', "-march=i686"]
     crt_defines['CMAKE_C_FLAGS'] = ' '.join(cflags)
     crt_defines['CMAKE_CXX_FLAGS'] = ' '.join(cflags)
 
@@ -558,11 +558,13 @@ def build_crts_host_i686(stage2_install, clang_version):
     crt_defines['COMPILER_RT_INCLUDE_TESTS'] = 'ON'
     crt_defines['COMPILER_RT_ENABLE_WERROR'] = 'ON'
     crt_defines['CMAKE_INSTALL_PREFIX'] = crt_install
-    crt_defines['SANITIZER_CXX_ABI'] = 'libc++'
+    crt_defines['SANITIZER_CXX_ABI'] = 'libstdc++'
+
+    crt_defines['COMPILER_RT_BUILD_LIBFUZZER'] = 'OFF'
 
     crt_env = dict(ORIG_ENV)
 
-    crt_path = utils.out_path('lib', 'clangrt-i686-host')
+    crt_path = utils.out_path('lib', 'clangrt-i386-host')
     rm_cmake_cache(crt_path)
     invoke_cmake(
         out_path=crt_path,