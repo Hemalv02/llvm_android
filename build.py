8c7fd88dfcca08376ba2c8272d027f5d01da6329
Pirama Arumuga Nainar <pirama@google.com>
2018-03-21 16:36:52 -0700

    Set LLVM_ENABLE_THREADS during toolchain build
    Bug: http://b/73400852
    
    This causes some LLVM operations (including ThinLTO compilation,
    llvm-profdata merge) to be multithreaded.
    
    Test: build.py and test_compiler.py in the master-plus-llvm branch
    Change-Id: Ie4021ddaefdfd2e2e30d5a508e1103ba4d74902e

--- build.py
+++ build.py
@@ -208,7 +208,7 @@ def base_cmake_defines():
     defines['LLVM_ENABLE_ASSERTIONS'] = 'OFF'
     # https://github.com/android-ndk/ndk/issues/574 - Don't depend on libtinfo.
     defines['LLVM_ENABLE_TERMINFO'] = 'OFF'
-    defines['LLVM_ENABLE_THREADS'] = 'OFF'
+    defines['LLVM_ENABLE_THREADS'] = 'ON'
     defines['LLVM_LIBDIR_SUFFIX'] = '64'
     defines['LLVM_VERSION_PATCH'] = android_version.patch_level
     defines['CLANG_VERSION_PATCHLEVEL'] = android_version.patch_level
