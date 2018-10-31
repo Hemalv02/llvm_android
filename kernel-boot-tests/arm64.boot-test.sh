aosp-riscv/toolchain_llvm_android

Add preliminary scripts for qualifying a prebuilt clang
A set of composeable shell scripts for providing quick feedback via
compile+boot testing of the Linux kernel with Clang.

Usage: cd kernel-boot-tests && qualify_clang.sh path/to/clang

Change-Id: I0efb0462505d243531513316cbd79c43798f9cac
Link: https://github.com/ClangBuiltLinux/linux/issues/2
Bug: 118506477

kernel-boot-tests/arm64.boot-test.sh
@@ -1,0 +1,28 @@
+#!/bin/bash
+# Usage ./arm64.boot-test.sh path/to/linux/Image
+set -ex
+
+run_qemu() {
+  local kernel_image=$1
+  local rootfs=$2
+
+  # verify images exist.
+  test -e $kernel_image
+  test -e $rootfs
+
+  # -no-reboot is expected for the rootfs, as it just runs a reboot if booted
+  # sucessfully.
+  timeout 10s unbuffer qemu-system-aarch64 \
+    -kernel $kernel_image \
+    -machine virt \
+    -cpu cortex-a57 \
+    -hda $rootfs \
+    -append "console=ttyAMA0 earlyprintk=ttyAMA0 root=/dev/vda" \
+    -m 512 \
+    -nographic \
+    -no-reboot
+}
+
+rootfs=arm64.rootfs.ext2
+./fetch.sh https://github.com/groeck/linux-build-test/blob/1105ee4079c78c576c3d88e33cf95420f620c116/rootfs/arm64/rootfs.ext2.gz?raw=true $rootfs dbe4136f0b4a0d2180b93fd2a3b9a784f9951d10
+run_qemu $1 $rootfs

kernel-boot-tests/fetch.sh
@@ -1,0 +1,25 @@
+#!/bin/bash
+# Usage: ./fetch.sh url output_filename [sha]
+# Note: the sha should be of the raw download, not post-gunzipped.
+#       sha's change after each boot of a fs image.
+set -ex
+
+fetch() {
+  local url=$1
+  local output_filename=$2
+  local sha=$3
+
+  # if the file does not exist, fetch it
+  if [[ ! -e $output_filename ]]; then
+    wget $url -O "$output_filename.gz"
+
+    # if a sha was provided, verify it
+    if [[ -n "$sha" ]]; then
+      echo "$sha $output_filename.gz" | sha1sum -c -
+    fi
+
+    gunzip "$output_filename.gz"
+  fi
+}
+
+fetch $@

kernel-boot-tests/qualify_clang.sh
@@ -1,0 +1,9 @@
+#!/bin/bash
+# Usage: ./qualify_clang.sh path/to/clang
+set -ex
+
+clang=$1
+
+test -n $clang
+./arm64.build-kernel.sh $clang
+./arm64.boot-test.sh linux/arch/arm64/boot/Image.gz

Hemalv02/llvm_android

kernel-boot-tests/arm64.boot-test.sh
@@ -1,0 +1,28 @@
+#!/bin/bash
+# Usage ./arm64.boot-test.sh path/to/linux/Image
+set -exu
+
+run_qemu() {
+  local kernel_image=$1
+  local rootfs=$2
+
+  # verify images exist.
+  test -e $kernel_image
+  test -e $rootfs
+
+  # -no-reboot is expected for the rootfs, as it just runs a reboot if booted
+  # sucessfully.
+  timeout 10s unbuffer qemu-system-aarch64 \
+    -kernel $kernel_image \
+    -machine virt \
+    -cpu cortex-a57 \
+    -hda $rootfs \
+    -append "console=ttyAMA0 earlyprintk=ttyAMA0 root=/dev/vda" \
+    -m 512 \
+    -nographic \
+    -no-reboot
+}
+
+rootfs=arm64.rootfs.ext2
+./fetch.sh https://github.com/groeck/linux-build-test/blob/1105ee4079c78c576c3d88e33cf95420f620c116/rootfs/arm64/rootfs.ext2.gz?raw=true $rootfs dbe4136f0b4a0d2180b93fd2a3b9a784f9951d10
+run_qemu $1 $rootfs

kernel-boot-tests/fetch.sh
@@ -1,0 +1,25 @@
+#!/bin/bash
+# Usage: ./fetch.sh url output_filename [sha]
+# Note: the sha should be of the raw download, not post-gunzipped.
+#       sha's change after each boot of a fs image.
+set -exu
+
+fetch() {
+  local url=$1
+  local output_filename=$2
+  local sha=$3
+
+  # if the file does not exist, fetch it
+  if [[ ! -e $output_filename ]]; then
+    wget $url -O "$output_filename.gz"
+
+    # if a sha was provided, verify it
+    if [[ -n "$sha" ]]; then
+      echo "$sha $output_filename.gz" | sha1sum -c -
+    fi
+
+    gunzip "$output_filename.gz"
+  fi
+}
+
+fetch $@

kernel-boot-tests/qualify_clang.sh
@@ -1,0 +1,10 @@
+#!/bin/bash
+# Usage: ./qualify_clang.sh path/to/clang
+set -exu
+
+clang=$1
+
+test -n $clang
+./check-dependencies.sh
+./arm64.build-kernel.sh $clang
+./arm64.boot-test.sh linux/arch/arm64/boot/Image.gz