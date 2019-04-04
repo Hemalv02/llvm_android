84646e530b78c51f6b79543917b2ce7cc310500b
Stephen Hines <srhines@google.com>
2019-04-03 19:47:32 -0700

    Use clang-r353983b to bootstrap Clang.
    Bug: http://b/128937182
    Test: build.py
    Change-Id: Icbc863af01e0d87168b2096d730df58b7d0e0563

--- build.py
+++ build.py
@@ -142,7 +142,7 @@ def support_headers():
 
 # This is the baseline stable version of Clang to start our stage-1 build.
 def clang_prebuilt_version():
-    return 'clang-r349610b'
+    return 'clang-r353983b'
 
 
 def clang_prebuilt_base_dir():
