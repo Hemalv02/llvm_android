ca4e74c5aef7ca50956fdee5aee57eeb6f7de0ad
Stephen Hines <srhines@google.com>
2020-07-07 20:22:31 -0700

    Add a missing patch to fix a Darwin flag issue with `-platform_version`.
    5122e82870 [driver][darwin] Don't use -platform_version flag by default (PR44813)
    
    Bug: http://b/160740292
    Test: ./build.py
    Change-Id: Iefa34e3b4969b3001499f55c47f3f2eb707058d0

--- android_version.py
+++ android_version.py
@@ -14,8 +14,8 @@
 # limitations under the License.
 #
 
-patch_level = '3'
-_svn_revision = 'r383902c'
+patch_level = '4'
+_svn_revision = 'r383902d'
 # svn_revision_next will be newer than the official svn_revision in the future.
 _svn_revision_next = 'r391452'
 
