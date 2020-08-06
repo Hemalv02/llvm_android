aosp-riscv/toolchain_llvm_android

Link libxml2 to lldb
Bug: 157157503
Change-Id: I8fe0698692bee20cd01794e7321fe5f1b72f02b5

base_builders.py
@@ -21,7 +21,7 @@ import multiprocessing
 import os
 import shutil
 import subprocess
-from typing import Dict, List, Optional, Set
+from typing import Dict, List, Optional, Set, Sequence
 
 import android_version
 from builder_registry import BuilderRegistry
@@ -43,8 +43,10 @@ class Builder:  # pylint: disable=too-few-public-methods
     name: str = ""
     config_list: List[configs.Config]
 
-    def __init__(self) -> None:
+    def __init__(self, config_list: Optional[Sequence[configs.Config]]=None) -> None:
         self._config: configs.Config
+        if config_list:
+            self.config_list = list(config_list)
 
     @BuilderRegistry.register_and_build
     def build(self) -> None:
@@ -103,12 +105,13 @@ class AutoconfBuilder(Builder):
     @property
     def output_dir(self) -> Path:
         """The path for intermediate results."""
-        return paths.OUT_DIR / 'lib' / (f'{self.name}')
+        return paths.OUT_DIR / 'lib' / (f'{self.name}{self._config.output_suffix}')
 
     @property
     def install_dir(self) -> Path:
         """Returns the path this target will be installed to."""
-        return paths.OUT_DIR / 'lib' / f'{self.name}-install'
+        output_dir = self.output_dir
+        return output_dir.parent / (output_dir.name + '-install')
 
     @staticmethod
     def _get_mac_sdk_path() -> Path:
@@ -198,12 +201,13 @@ class CMakeBuilder(Builder):
     @property
     def output_dir(self) -> Path:
         """The path for intermediate results."""
-        return paths.OUT_DIR / 'lib' / f'{self.name}'
+        return paths.OUT_DIR / 'lib' / (f'{self.name}{self._config.output_suffix}')
 
     @property
     def install_dir(self) -> Path:
         """Returns the path this target will be installed to."""
-        return paths.OUT_DIR / 'lib' / f'{self.name}-install'
+        output_dir = self.output_dir
+        return output_dir.parent / (output_dir.name + '-install')
 
     @property
     def toolchain(self) -> toolchains.Toolchain:
@@ -231,7 +235,7 @@ class CMakeBuilder(Builder):
             'CMAKE_OBJCOPY': str(self.toolchain.objcopy),
             'CMAKE_OBJDUMP': str(self.toolchain.objdump),
             'CMAKE_RANLIB': str(self.toolchain.ranlib),
-            'CMAKE_RC': str(self.toolchain.rc),
+            'CMAKE_RC_COMPILER': str(self.toolchain.rc),
             'CMAKE_READELF': str(self.toolchain.readelf),
             'CMAKE_STRIP': str(self.toolchain.strip),
             'CMAKE_MT': str(self.toolchain.mt),
@@ -380,10 +384,6 @@ class LLVMRuntimeBuilder(LLVMBaseBuilder):  # pylint: disable=abstract-method
         return self.toolchain.path / 'runtimes_ndk_cxx' / arch.value
 
     @property
-    def output_dir(self) -> Path:
-        return paths.OUT_DIR / 'lib' / (f'{self.name}{self._config.output_suffix}')
-
-    @property
     def cmake_defines(self) -> Dict[str, str]:
         defines: Dict[str, str] = super().cmake_defines
         defines['LLVM_CONFIG_PATH'] = str(self.toolchain.path /
@@ -400,6 +400,13 @@ class LLVMBuilder(LLVMBaseBuilder):
     svn_revision: str
     enable_assertions: bool = False
     toolchain_name: str
+
+    # lldb options.
+    build_lldb: bool = True
+    libxml2_path: Optional[Path] = None
+    swig_executable: Optional[Path] = None
+    liblzma_path: Optional[Path] = None
+    libedit_path: Optional[Path] = None
 
     @property
     def toolchain(self) -> toolchains.Toolchain:
@@ -423,43 +430,47 @@ class LLVMBuilder(LLVMBaseBuilder):
         """Returns llvm target archtects to build."""
         raise NotImplementedError()
 
-    @property
-    def _enable_lldb(self) -> bool:
-        return 'lldb' in self.llvm_projects
-
     def set_lldb_flags(self, target: hosts.Host, defines: Dict[str, str]) -> None:  # pylint: disable=no-self-use
         """Sets cmake defines for lldb."""
-        # By default all these are auto-detected. Disable them explicitly to avoid unexpected dependency.
-        defines['LLDB_ENABLE_LIBXML2'] = 'OFF'
-        defines['LLDB_ENABLE_LUA'] = 'OFF'
-
-        swig_executable = BuilderRegistry.get('swig').install_dir / 'bin' / 'swig'
-        defines['SWIG_EXECUTABLE'] = str(swig_executable)
-        py_prefix = 'Python3' if target.is_windows else 'PYTHON'
-        defines['LLDB_ENABLE_PYTHON'] = 'ON'
-        defines[f'{py_prefix}_LIBRARY'] = str(paths.get_python_lib(target))
-        defines[f'{py_prefix}_LIBRARIES'] = str(paths.get_python_lib(target))
-        defines[f'{py_prefix}_INCLUDE_DIR'] = str(paths.get_python_include_dir(target))
-        defines[f'{py_prefix}_INCLUDE_DIRS'] = str(paths.get_python_include_dir(target))
-        defines[f'{py_prefix}_EXECUTABLE'] = str(paths.get_python_executable(hosts.build_host()))
-        defines['LLDB_EMBED_PYTHON_HOME'] = 'OFF'
-
         if target.is_darwin:
             # Avoids the build of debug server. It is only used in testing.
             defines['LLDB_USE_SYSTEM_DEBUGSERVER'] = 'ON'
 
-        defines['LLDB_ENABLE_LZMA'] = 'ON'
-        xz_root = BuilderRegistry.get('xz-windows' if target.is_windows else 'xz').install_dir
-        defines['LIBLZMA_INCLUDE_DIR'] = str(xz_root / 'include')
-        defines['LIBLZMA_LIBRARY'] = str(xz_root / 'lib' / 'liblzma.a')
+        defines['LLDB_ENABLE_LUA'] = 'OFF'
 
-        if not target.is_windows:
-            libedit_root = BuilderRegistry.get('libedit').install_dir
+        if self.swig_executable:
+            defines['SWIG_EXECUTABLE'] = str(self.swig_executable)
+            py_prefix = 'Python3' if target.is_windows else 'PYTHON'
+            defines['LLDB_ENABLE_PYTHON'] = 'ON'
+            defines[f'{py_prefix}_LIBRARY'] = str(paths.get_python_lib(target))
+            defines[f'{py_prefix}_LIBRARIES'] = str(paths.get_python_lib(target))
+            defines[f'{py_prefix}_INCLUDE_DIR'] = str(paths.get_python_include_dir(target))
+            defines[f'{py_prefix}_INCLUDE_DIRS'] = str(paths.get_python_include_dir(target))
+            defines[f'{py_prefix}_EXECUTABLE'] = str(paths.get_python_executable(hosts.build_host()))
+            defines['LLDB_EMBED_PYTHON_HOME'] = 'OFF'
+        else:
+            defines['LLDB_ENABLE_PYTHON'] = 'OFF'
+
+        if self.liblzma_path:
+            defines['LLDB_ENABLE_LZMA'] = 'ON'
+            defines['LIBLZMA_INCLUDE_DIR'] = str(self.liblzma_path / 'include')
+            defines['LIBLZMA_LIBRARY'] = str(self.liblzma_path / 'lib' / 'liblzma.a')
+        else:
+            defines['LLDB_ENABLE_LZMA'] = 'OFF'
+
+        if self.libedit_path:
             defines['LLDB_ENABLE_LIBEDIT'] = 'ON'
-            defines['LibEdit_INCLUDE_DIRS'] = str(paths.get_libedit_include_dir(libedit_root))
-            defines['LibEdit_LIBRARIES'] = str(paths.get_libedit_lib(libedit_root, target))
+            defines['LibEdit_INCLUDE_DIRS'] = str(self.libedit_path / 'include')
+            defines['LibEdit_LIBRARIES'] = str(paths.get_libedit_lib(self.libedit_path, target))
         else:
             defines['LLDB_ENABLE_LIBEDIT'] = 'OFF'
+
+        if self.libxml2_path:
+            defines['LLDB_ENABLE_LIBXML2'] = 'ON'
+            defines['LIBXML2_INCLUDE_DIR'] = str(self.libxml2_path / 'include' / 'libxml2')
+            defines['LIBXML2_LIBRARY'] = str(paths.get_libxml2_lib(self.libxml2_path, target))
+        else:
+            defines['LLDB_ENABLE_LIBXML2'] = 'OFF'
 
     @property
     def cmake_defines(self) -> Dict[str, str]:
@@ -477,7 +488,7 @@ class LLVMBuilder(LLVMBaseBuilder):
                                               'binutils' / 'binutils-2.27' / 'include')
         defines['LLVM_BUILD_RUNTIME'] = 'ON'
 
-        if self._enable_lldb:
+        if self.build_lldb:
             self.set_lldb_flags(self._config.target_os, defines)
 
         return defines

do_build.py
@@ -455,10 +455,11 @@ class SysrootsBuilder(base_builders.Builder):
                 shutil.copy2(out_path, libdir)
 
 
-def build_llvm_for_windows(enable_assertions,
-                           build_name):
-    if BUILD_LLDB:
-        builders.XzWindowsBuilder().build()
+def build_llvm_for_windows(enable_assertions: bool,
+                           build_name: str,
+                           swig_builder: Optional[builders.SwigBuilder]):
+    config_list = [configs.WindowsConfig()]
+    lldb_deps_to_install: List[Path] = []
 
     win_builder = builders.WindowsToolchainBuilder()
     if win_builder.install_dir.exists():
@@ -473,13 +474,26 @@ def build_llvm_for_windows(enable_assertions,
     libcxx_builder.enable_assertions = enable_assertions
     libcxx_builder.build()
 
+    win_builder.build_lldb = BUILD_LLDB
+    if BUILD_LLDB:
+        assert swig_builder is not None
+        win_builder.libedit_path = None
+        win_builder.swig_executable = swig_builder.install_dir / 'bin' / 'swig'
+        xz_builder = builders.XzBuilder(config_list)
+        xz_builder.build()
+        win_builder.liblzma_path = xz_builder.install_dir
+        libxml2_builder = builders.LibXml2Builder(config_list)
+        libxml2_builder.build()
+        win_builder.libxml2_path = libxml2_builder.install_dir
+        lldb_deps_to_install.append(
+            paths.get_libxml2_lib(libxml2_builder.install_dir, hosts.Host.Windows))
+
     win_builder.build_name = build_name
     win_builder.svn_revision = android_version.get_svn_revision(BUILD_LLVM_NEXT)
-    win_builder.build_lldb = BUILD_LLDB
     win_builder.enable_assertions = enable_assertions
     win_builder.build()
 
-    return win_builder.install_dir
+    return (win_builder.install_dir, lldb_deps_to_install)
 
 
 def host_sysroot():
@@ -527,7 +541,7 @@ def host_gcc_toolchain_flags(host: hosts.Host, is_32_bit=False):
     return cflags, ldflags
 
 
-def install_lldb_deps(install_dir: Path, host: hosts.Host):
+def install_lldb_deps(install_dir: Path, host: hosts.Host, lldb_deps: List[Path]):
     lib_dir = install_dir / ('bin' if host.is_windows else 'lib64')
     check_create_path(lib_dir)
 
@@ -541,9 +555,9 @@ def install_lldb_deps(install_dir: Path, host: hosts.Host):
     dest_py_lib = python_dest_dir / py_lib
     py_lib_rel = os.path.relpath(dest_py_lib, lib_dir)
     os.symlink(py_lib_rel, lib_dir / py_lib.name)
-    if not host.is_windows:
-        libedit_root = BuilderRegistry.get('libedit').install_dir
-        shutil.copy2(paths.get_libedit_lib(libedit_root, host), lib_dir)
+
+    for lldb_dep in lldb_deps:
+        shutil.copy(lldb_dep, lib_dir)
 
 
 def build_runtimes(toolchain, args=None):
@@ -720,7 +734,8 @@ def get_package_install_path(host: hosts.Host, package_name):
     return utils.out_path('install', host.os_tag, package_name)
 
 
-def package_toolchain(build_dir, build_name, host: hosts.Host, dist_dir, strip=True, create_tar=True):
+def package_toolchain(build_dir, build_name, host: hosts.Host, dist_dir,
+                      lldb_deps_to_install: List[Path], strip=True, create_tar=True):
     package_name = 'clang-' + build_name
     version = extract_clang_version(build_dir)
 
@@ -799,13 +814,13 @@ def package_toolchain(build_dir, build_name, host: hosts.Host, dist_dir, strip=T
         necessary_bin_files -= windows_exclude_bin_files
 
     if BUILD_LLDB:
-        install_lldb_deps(Path(install_dir), host)
+        install_lldb_deps(Path(install_dir), host, lldb_deps_to_install)
         if host.is_windows:
-            windows_additional_bin_files = {
+            necessary_bin_files |= {
                 'liblldb' + shlib_ext,
                 'python38' + shlib_ext
             }
-            necessary_bin_files |= windows_additional_bin_files
+            necessary_bin_files |= set(path.name for path in lldb_deps_to_install)
 
     # scripts that should not be stripped
     script_bins = {
@@ -1072,11 +1087,11 @@ def main():
     stage1_install = str(stage1.install_dir)
 
     if BUILD_LLDB:
-        builders.SwigBuilder().build()
-        if need_host:
-            builders.XzBuilder().build()
-            # libedit is not needed for windows lldb.
-            builders.LibEditBuilder().build()
+        # Swig is needed for both host and windows lldb.
+        swig_builder = builders.SwigBuilder()
+        swig_builder.build()
+    else:
+        swig_builder = None
 
     if need_host:
         profdata_filename = pgo_profdata_filename()
@@ -1085,12 +1100,27 @@ def main():
         stage2 = builders.Stage2Builder()
         stage2.build_name = args.build_name
         stage2.svn_revision = android_version.get_svn_revision(BUILD_LLVM_NEXT)
-        stage2.build_lldb = BUILD_LLDB
         stage2.debug_build = args.debug
         stage2.enable_assertions = args.enable_assertions
         stage2.lto = not args.no_lto
         stage2.build_instrumented = instrumented
         stage2.profdata_file = Path(profdata) if profdata else None
+
+        stage2.build_lldb = BUILD_LLDB
+        if BUILD_LLDB:
+            lldb_deps: List[Path] = []
+            stage2.swig_executable = swig_builder.install_dir / 'bin' / 'swig'
+            xz_builder = builders.XzBuilder()
+            xz_builder.build()
+            stage2.liblzma_path = xz_builder.install_dir
+            libxml2_builder = builders.LibXml2Builder()
+            libxml2_builder.build()
+            lldb_deps.append(paths.get_libxml2_lib(libxml2_builder.install_dir, hosts.build_host()))
+            stage2.libxml2_path = libxml2_builder.install_dir
+            libedit_builder = builders.LibEditBuilder()
+            libedit_builder.build()
+            stage2.libedit_path = libedit_builder.install_dir
+            lldb_deps.append(paths.get_libedit_lib(libedit_builder.install_dir, hosts.build_host()))
 
         # Annotate the version string if there is no profdata.
         if profdata is None:
@@ -1112,9 +1142,10 @@ def main():
             build_runtimes(runtimes_toolchain, args)
 
     if need_windows:
-        windows64_install = build_llvm_for_windows(
+        windows64_install, win_lldb_deps = build_llvm_for_windows(
             enable_assertions=args.enable_assertions,
-            build_name=args.build_name)
+            build_name=args.build_name,
+            swig_builder=swig_builder)
 
     dist_dir = ORIG_ENV.get('DIST_DIR', utils.out_path())
     if do_package and need_host:
@@ -1123,6 +1154,7 @@ def main():
             args.build_name,
             hosts.build_host(),
             dist_dir,
+            lldb_deps,
             strip=do_strip_host_package)
 
     if do_package and need_windows:
@@ -1131,6 +1163,7 @@ def main():
             args.build_name,
             hosts.Host.Windows,
             dist_dir,
+            win_lldb_deps,
             strip=do_strip)
 
     return 0

Hemalv02/llvm_android

base_builders.py
@@ -21,7 +21,7 @@ import multiprocessing
 import os
 import shutil
 import subprocess
-from typing import Dict, List, Optional, Set
+from typing import Dict, List, Optional, Set, Sequence
 
 import android_version
 from builder_registry import BuilderRegistry
@@ -38,13 +38,35 @@ def logger():
     """Returns the module level logger."""
     return logging.getLogger(__name__)
 
+
+class LibInfo:
+    """An interface to get information of a library."""
+
+    @property
+    def include_dir(self) -> Path:
+        """Path to headers."""
+        raise NotImplementedError()
+
+    @property
+    def link_library(self) -> Path:
+        """Path to the library used when linking."""
+        raise NotImplementedError()
+
+    @property
+    def install_library(self) -> Optional[Path]:
+        """Path to the library to install. Returns None for static library."""
+        raise NotImplementedError()
+
+
 class Builder:  # pylint: disable=too-few-public-methods
     """Base builder type."""
     name: str = ""
     config_list: List[configs.Config]
 
-    def __init__(self) -> None:
+    def __init__(self, config_list: Optional[Sequence[configs.Config]]=None) -> None:
         self._config: configs.Config
+        if config_list:
+            self.config_list = list(config_list)
 
     @BuilderRegistry.register_and_build
     def build(self) -> None:
@@ -103,12 +125,13 @@ class AutoconfBuilder(Builder):
     @property
     def output_dir(self) -> Path:
         """The path for intermediate results."""
-        return paths.OUT_DIR / 'lib' / (f'{self.name}')
+        return paths.OUT_DIR / 'lib' / (f'{self.name}{self._config.output_suffix}')
 
     @property
     def install_dir(self) -> Path:
         """Returns the path this target will be installed to."""
-        return paths.OUT_DIR / 'lib' / f'{self.name}-install'
+        output_dir = self.output_dir
+        return output_dir.parent / (output_dir.name + '-install')
 
     @staticmethod
     def _get_mac_sdk_path() -> Path:
@@ -198,12 +221,13 @@ class CMakeBuilder(Builder):
     @property
     def output_dir(self) -> Path:
         """The path for intermediate results."""
-        return paths.OUT_DIR / 'lib' / f'{self.name}'
+        return paths.OUT_DIR / 'lib' / (f'{self.name}{self._config.output_suffix}')
 
     @property
     def install_dir(self) -> Path:
         """Returns the path this target will be installed to."""
-        return paths.OUT_DIR / 'lib' / f'{self.name}-install'
+        output_dir = self.output_dir
+        return output_dir.parent / (output_dir.name + '-install')
 
     @property
     def toolchain(self) -> toolchains.Toolchain:
@@ -231,7 +255,7 @@ class CMakeBuilder(Builder):
             'CMAKE_OBJCOPY': str(self.toolchain.objcopy),
             'CMAKE_OBJDUMP': str(self.toolchain.objdump),
             'CMAKE_RANLIB': str(self.toolchain.ranlib),
-            'CMAKE_RC': str(self.toolchain.rc),
+            'CMAKE_RC_COMPILER': str(self.toolchain.rc),
             'CMAKE_READELF': str(self.toolchain.readelf),
             'CMAKE_STRIP': str(self.toolchain.strip),
             'CMAKE_MT': str(self.toolchain.mt),
@@ -380,10 +404,6 @@ class LLVMRuntimeBuilder(LLVMBaseBuilder):  # pylint: disable=abstract-method
         return self.toolchain.path / 'runtimes_ndk_cxx' / arch.value
 
     @property
-    def output_dir(self) -> Path:
-        return paths.OUT_DIR / 'lib' / (f'{self.name}{self._config.output_suffix}')
-
-    @property
     def cmake_defines(self) -> Dict[str, str]:
         defines: Dict[str, str] = super().cmake_defines
         defines['LLVM_CONFIG_PATH'] = str(self.toolchain.path /
@@ -400,6 +420,13 @@ class LLVMBuilder(LLVMBaseBuilder):
     svn_revision: str
     enable_assertions: bool = False
     toolchain_name: str
+
+    # lldb options.
+    build_lldb: bool = True
+    swig_executable: Optional[Path] = None
+    libxml2: Optional[LibInfo] = None
+    liblzma: Optional[LibInfo] = None
+    libedit: Optional[LibInfo] = None
 
     @property
     def toolchain(self) -> toolchains.Toolchain:
@@ -423,43 +450,47 @@ class LLVMBuilder(LLVMBaseBuilder):
         """Returns llvm target archtects to build."""
         raise NotImplementedError()
 
-    @property
-    def _enable_lldb(self) -> bool:
-        return 'lldb' in self.llvm_projects
-
     def set_lldb_flags(self, target: hosts.Host, defines: Dict[str, str]) -> None:  # pylint: disable=no-self-use
         """Sets cmake defines for lldb."""
-        # By default all these are auto-detected. Disable them explicitly to avoid unexpected dependency.
-        defines['LLDB_ENABLE_LIBXML2'] = 'OFF'
-        defines['LLDB_ENABLE_LUA'] = 'OFF'
-
-        swig_executable = BuilderRegistry.get('swig').install_dir / 'bin' / 'swig'
-        defines['SWIG_EXECUTABLE'] = str(swig_executable)
-        py_prefix = 'Python3' if target.is_windows else 'PYTHON'
-        defines['LLDB_ENABLE_PYTHON'] = 'ON'
-        defines[f'{py_prefix}_LIBRARY'] = str(paths.get_python_lib(target))
-        defines[f'{py_prefix}_LIBRARIES'] = str(paths.get_python_lib(target))
-        defines[f'{py_prefix}_INCLUDE_DIR'] = str(paths.get_python_include_dir(target))
-        defines[f'{py_prefix}_INCLUDE_DIRS'] = str(paths.get_python_include_dir(target))
-        defines[f'{py_prefix}_EXECUTABLE'] = str(paths.get_python_executable(hosts.build_host()))
-        defines['LLDB_EMBED_PYTHON_HOME'] = 'OFF'
-
         if target.is_darwin:
             # Avoids the build of debug server. It is only used in testing.
             defines['LLDB_USE_SYSTEM_DEBUGSERVER'] = 'ON'
 
-        defines['LLDB_ENABLE_LZMA'] = 'ON'
-        xz_root = BuilderRegistry.get('xz-windows' if target.is_windows else 'xz').install_dir
-        defines['LIBLZMA_INCLUDE_DIR'] = str(xz_root / 'include')
-        defines['LIBLZMA_LIBRARY'] = str(xz_root / 'lib' / 'liblzma.a')
+        defines['LLDB_ENABLE_LUA'] = 'OFF'
 
-        if not target.is_windows:
-            libedit_root = BuilderRegistry.get('libedit').install_dir
+        if self.swig_executable:
+            defines['SWIG_EXECUTABLE'] = str(self.swig_executable)
+            py_prefix = 'Python3' if target.is_windows else 'PYTHON'
+            defines['LLDB_ENABLE_PYTHON'] = 'ON'
+            defines[f'{py_prefix}_LIBRARY'] = str(paths.get_python_lib(target))
+            defines[f'{py_prefix}_LIBRARIES'] = str(paths.get_python_lib(target))
+            defines[f'{py_prefix}_INCLUDE_DIR'] = str(paths.get_python_include_dir(target))
+            defines[f'{py_prefix}_INCLUDE_DIRS'] = str(paths.get_python_include_dir(target))
+            defines[f'{py_prefix}_EXECUTABLE'] = str(paths.get_python_executable(hosts.build_host()))
+            defines['LLDB_EMBED_PYTHON_HOME'] = 'OFF'
+        else:
+            defines['LLDB_ENABLE_PYTHON'] = 'OFF'
+
+        if self.liblzma:
+            defines['LLDB_ENABLE_LZMA'] = 'ON'
+            defines['LIBLZMA_INCLUDE_DIR'] = str(self.liblzma.include_dir)
+            defines['LIBLZMA_LIBRARY'] = str(self.liblzma.link_library)
+        else:
+            defines['LLDB_ENABLE_LZMA'] = 'OFF'
+
+        if self.libedit:
             defines['LLDB_ENABLE_LIBEDIT'] = 'ON'
-            defines['LibEdit_INCLUDE_DIRS'] = str(paths.get_libedit_include_dir(libedit_root))
-            defines['LibEdit_LIBRARIES'] = str(paths.get_libedit_lib(libedit_root, target))
+            defines['LibEdit_INCLUDE_DIRS'] = str(self.libedit.include_dir)
+            defines['LibEdit_LIBRARIES'] = str(self.libedit.link_library)
         else:
             defines['LLDB_ENABLE_LIBEDIT'] = 'OFF'
+
+        if self.libxml2:
+            defines['LLDB_ENABLE_LIBXML2'] = 'ON'
+            defines['LIBXML2_INCLUDE_DIR'] = str(self.libxml2.include_dir)
+            defines['LIBXML2_LIBRARY'] = str(self.libxml2.link_library)
+        else:
+            defines['LLDB_ENABLE_LIBXML2'] = 'OFF'
 
     @property
     def cmake_defines(self) -> Dict[str, str]:
@@ -482,7 +513,7 @@ class LLVMBuilder(LLVMBaseBuilder):
                 raise RuntimeError('libcompression can be enabled for macOS 10.11 and above.')
             defines['HAVE_LIBCOMPRESSION'] = '0'
 
-        if self._enable_lldb:
+        if self.build_lldb:
             self.set_lldb_flags(self._config.target_os, defines)
 
         return defines

do_build.py
@@ -455,10 +455,11 @@ class SysrootsBuilder(base_builders.Builder):
                 shutil.copy2(out_path, libdir)
 
 
-def build_llvm_for_windows(enable_assertions,
-                           build_name):
-    if BUILD_LLDB:
-        builders.XzWindowsBuilder().build()
+def build_llvm_for_windows(enable_assertions: bool,
+                           build_name: str,
+                           swig_builder: Optional[builders.SwigBuilder]):
+    config_list = [configs.WindowsConfig()]
+    lldb_deps_to_install: List[Path] = []
 
     win_builder = builders.WindowsToolchainBuilder()
     if win_builder.install_dir.exists():
@@ -473,13 +474,27 @@ def build_llvm_for_windows(enable_assertions,
     libcxx_builder.enable_assertions = enable_assertions
     libcxx_builder.build()
 
+    win_builder.build_lldb = BUILD_LLDB
+    if BUILD_LLDB:
+        assert swig_builder is not None
+        win_builder.libedit = None
+        win_builder.swig_executable = swig_builder.install_dir / 'bin' / 'swig'
+
+        xz_builder = builders.XzBuilder(config_list)
+        xz_builder.build()
+        win_builder.liblzma = xz_builder
+
+        libxml2_builder = builders.LibXml2Builder(config_list)
+        libxml2_builder.build()
+        win_builder.libxml2 = libxml2_builder
+        lldb_deps_to_install.append(libxml2_builder.install_library)
+
     win_builder.build_name = build_name
     win_builder.svn_revision = android_version.get_svn_revision(BUILD_LLVM_NEXT)
-    win_builder.build_lldb = BUILD_LLDB
     win_builder.enable_assertions = enable_assertions
     win_builder.build()
 
-    return win_builder.install_dir
+    return (win_builder.install_dir, lldb_deps_to_install)
 
 
 def host_sysroot():
@@ -527,7 +542,7 @@ def host_gcc_toolchain_flags(host: hosts.Host, is_32_bit=False):
     return cflags, ldflags
 
 
-def install_lldb_deps(install_dir: Path, host: hosts.Host):
+def install_lldb_deps(install_dir: Path, host: hosts.Host, lldb_deps: List[Path]):
     lib_dir = install_dir / ('bin' if host.is_windows else 'lib64')
     check_create_path(lib_dir)
 
@@ -541,9 +556,9 @@ def install_lldb_deps(install_dir: Path, host: hosts.Host):
     dest_py_lib = python_dest_dir / py_lib
     py_lib_rel = os.path.relpath(dest_py_lib, lib_dir)
     os.symlink(py_lib_rel, lib_dir / py_lib.name)
-    if not host.is_windows:
-        libedit_root = BuilderRegistry.get('libedit').install_dir
-        shutil.copy2(paths.get_libedit_lib(libedit_root, host), lib_dir)
+
+    for lldb_dep in lldb_deps:
+        shutil.copy(lldb_dep, lib_dir)
 
 
 def build_runtimes(toolchain, args=None):
@@ -720,7 +735,8 @@ def get_package_install_path(host: hosts.Host, package_name):
     return utils.out_path('install', host.os_tag, package_name)
 
 
-def package_toolchain(build_dir, build_name, host: hosts.Host, dist_dir, strip=True, create_tar=True):
+def package_toolchain(build_dir, build_name, host: hosts.Host, dist_dir,
+                      lldb_deps_to_install: List[Path], strip=True, create_tar=True):
     package_name = 'clang-' + build_name
     version = extract_clang_version(build_dir)
 
@@ -799,13 +815,13 @@ def package_toolchain(build_dir, build_name, host: hosts.Host, dist_dir, strip=T
         necessary_bin_files -= windows_exclude_bin_files
 
     if BUILD_LLDB:
-        install_lldb_deps(Path(install_dir), host)
+        install_lldb_deps(Path(install_dir), host, lldb_deps_to_install)
         if host.is_windows:
-            windows_additional_bin_files = {
+            necessary_bin_files |= {
                 'liblldb' + shlib_ext,
                 'python38' + shlib_ext
             }
-            necessary_bin_files |= windows_additional_bin_files
+            necessary_bin_files |= set(path.name for path in lldb_deps_to_install)
 
     # scripts that should not be stripped
     script_bins = {
@@ -1072,11 +1088,11 @@ def main():
     stage1_install = str(stage1.install_dir)
 
     if BUILD_LLDB:
-        builders.SwigBuilder().build()
-        if need_host:
-            builders.XzBuilder().build()
-            # libedit is not needed for windows lldb.
-            builders.LibEditBuilder().build()
+        # Swig is needed for both host and windows lldb.
+        swig_builder = builders.SwigBuilder()
+        swig_builder.build()
+    else:
+        swig_builder = None
 
     if need_host:
         profdata_filename = pgo_profdata_filename()
@@ -1085,12 +1101,32 @@ def main():
         stage2 = builders.Stage2Builder()
         stage2.build_name = args.build_name
         stage2.svn_revision = android_version.get_svn_revision(BUILD_LLVM_NEXT)
-        stage2.build_lldb = BUILD_LLDB
         stage2.debug_build = args.debug
         stage2.enable_assertions = args.enable_assertions
         stage2.lto = not args.no_lto
         stage2.build_instrumented = instrumented
         stage2.profdata_file = Path(profdata) if profdata else None
+
+        stage2.build_lldb = BUILD_LLDB
+        if BUILD_LLDB:
+            stage2.swig_executable = swig_builder.install_dir / 'bin' / 'swig'
+
+            xz_builder = builders.XzBuilder()
+            xz_builder.build()
+            stage2.liblzma = xz_builder
+
+            libxml2_builder = builders.LibXml2Builder()
+            libxml2_builder.build()
+            stage2.libxml2_path = libxml2_builder
+
+            libedit_builder = builders.LibEditBuilder()
+            libedit_builder.build()
+            stage2.libedit = libedit_builder
+
+            lldb_deps: List[Path] = [
+                libxml2_builder.install_library,
+                libedit_builder.install_library,
+            ]
 
         # Annotate the version string if there is no profdata.
         if profdata is None:
@@ -1112,9 +1148,10 @@ def main():
             build_runtimes(runtimes_toolchain, args)
 
     if need_windows:
-        windows64_install = build_llvm_for_windows(
+        windows64_install, win_lldb_deps = build_llvm_for_windows(
             enable_assertions=args.enable_assertions,
-            build_name=args.build_name)
+            build_name=args.build_name,
+            swig_builder=swig_builder)
 
     dist_dir = ORIG_ENV.get('DIST_DIR', utils.out_path())
     if do_package and need_host:
@@ -1123,6 +1160,7 @@ def main():
             args.build_name,
             hosts.build_host(),
             dist_dir,
+            lldb_deps,
             strip=do_strip_host_package)
 
     if do_package and need_windows:
@@ -1131,6 +1169,7 @@ def main():
             args.build_name,
             hosts.Host.Windows,
             dist_dir,
+            win_lldb_deps,
             strip=do_strip)
 
     return 0