d438b35b673ab6d6d4dca4e68a463988079790f3
Stephen Hines <srhines@google.com>
2020-09-21 19:11:46 -0700

    Bump to r383902b1.
    Bug: http://b/168178182
    Test: ./build.py
    Change-Id: I90a33079ab9ef575082a884101613b2f15ccd8a9

--- android_version.py
+++ android_version.py
@@ -17,7 +17,7 @@
 import re
 
 patch_level = '2'
-_svn_revision = 'r383902b'
+_svn_revision = 'r383902b1'
 # svn_revision_next will be newer than the official svn_revision in the future.
 _svn_revision_next = 'r383902'
 
