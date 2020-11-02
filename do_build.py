f3a0eeba8a12fcf3cdd6927bb8859b727e420495
Pirama Arumuga Nainar <pirama@google.com>
2020-11-02 15:50:40 -0800

    For llvm_next builds: enable assertions, do not strip
    Bug: http://b/161849352
    
    Test: build.py --build-llvm-next
    Change-Id: I25945286c74833dd78ba77d8c1c35d95558b25cc

--- do_build.py
+++ do_build.py
@@ -590,7 +590,8 @@ def main():
     do_runtimes = not args.skip_runtimes
     do_package = not args.skip_package
     do_strip = not args.no_strip
-    do_strip_host_package = do_strip and not args.debug
+    do_strip_host_package = do_strip and not args.debug and not args.build_llvm_next
+    enable_assertions = args.enable_assertions or args.build_llvm_next
     build_lldb = 'lldb' not in args.no_build
 
     android_version.set_llvm_next(args.build_llvm_next)
@@ -634,7 +635,7 @@ def main():
         stage2.build_name = args.build_name
         stage2.svn_revision = android_version.get_svn_revision()
         stage2.debug_build = args.debug
-        stage2.enable_assertions = args.enable_assertions
+        stage2.enable_assertions = enable_assertions
         stage2.lto = not args.no_lto
         stage2.build_instrumented = instrumented
         stage2.profdata_file = profdata if profdata else None
@@ -676,7 +677,7 @@ def main():
         if args.windows_sdk:
             win_sdk.set_path(Path(args.windows_sdk))
         win_builder, win_lldb_bins = build_llvm_for_windows(
-            enable_assertions=args.enable_assertions,
+            enable_assertions=enable_assertions,
             build_name=args.build_name,
             build_lldb=build_lldb,
             swig_builder=swig_builder)
