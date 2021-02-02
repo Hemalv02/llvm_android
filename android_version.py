d80ed13766fb2464f1a393d95b2b73ee0c780174
Chih-Hung Hsieh <chh@google.com>
2021-02-01 20:14:24 -0800

    Update llvm to r412851.
    Bug: 179065588
    Test: build.py and test_compiler.py.
    Change-Id: I1d4a69d02fc29d11cd2e673ddc8e1ae72d8bdf2b

--- android_version.py
+++ android_version.py
@@ -19,8 +19,8 @@ import re
 _llvm_next = False
 _version_read = False
 
-_patch_level = '2'
-_svn_revision = 'r407598b'
+_patch_level = '3'
+_svn_revision = 'r412851'
 # svn_revision_next will be newer than the official svn_revision in the future.
 _svn_revision_next = 'r412851'
 
