d193f2b8b7dccc21a8e6523e498f0f2e4cb90460
Yi Kong <yikong@google.com>
2021-06-03 11:42:52 +0800

    Update toolchain for stage1 to clang-r416183c
    Test: presubmit
    Change-Id: I41dfb58af01ef266cb34b3ab937be3610a618a15

--- constants.py
+++ constants.py
@@ -21,7 +21,7 @@ from typing import Set
 MAC_MIN_VERSION: str = '10.10'
 
 # This is the baseline stable version of Clang to start our stage-1 build.
-CLANG_PREBUILT_VERSION: str = 'clang-bootstrap'
+CLANG_PREBUILT_VERSION: str = 'clang-r416183c'
 
 # This is the ndk version used to build runtimes.
 NDK_VERSION: str = 'r23'
