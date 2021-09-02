b927e4123ee51c34efeab3cc83eff3cb9e1a7d9d
Pirama Arumuga Nainar <pirama@google.com>
2021-09-01 21:01:37 -0700

    Bump to r416183c1
    Bug: http://b/198330183
    
    for NDK r23b toolchain respin
    
    Test: presubmit
    Change-Id: Ifaf7344f72c3c91c5c2a1dcbfcb573be536a8610

--- android_version.py
+++ android_version.py
@@ -19,8 +19,8 @@ import re
 _llvm_next = False
 _version_read = False
 
-_patch_level = '6'
-_svn_revision = 'r416183c'
+_patch_level = '8'
+_svn_revision = 'r416183c1'
 # svn_revision_next will be newer than the official svn_revision in the future.
 _svn_revision_next = 'r416183'
 
