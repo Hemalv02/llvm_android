a9f1f260f1bbdbeedb8fe15028b07e867706b913
Stephen Hines <srhines@google.com>
2021-10-05 13:38:58 -0700

    Bump version to r433403b.
    Bug: http://b/202099787
    Test: ./build.py
    Change-Id: I9fc04eca9f8ea9f97f7b06f7cb5fd5cc3807f3ed

--- android_version.py
+++ android_version.py
@@ -19,8 +19,8 @@ import re
 _llvm_next = False
 _version_read = False
 
-_patch_level = '2'
-_svn_revision = 'r433403'
+_patch_level = '3'
+_svn_revision = 'r433403b'
 # svn_revision_next will be newer than the official svn_revision in the future.
 _svn_revision_next = 'r433403'
 
