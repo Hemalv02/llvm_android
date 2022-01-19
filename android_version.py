3ee1d6b4a6c758ce1ec37c43799f5d7c611c75dd
Pirama Arumuga Nainar <pirama@google.com>
2022-01-18 22:23:31 -0800

    Update testing branch to r445002
    Bug: http://b/214080353
    
    - TSAN: Add patch for pending upstream fix that restricts tsan shared library
    build.
    - TSAN: Set ANDROID_PLATFORM_LEVEL so to get the above patch to fire.
    - libc++: Cherry-pick 89103bd2f06d to fix test failure with gdb built
    with python 2.7.
    - lldb: Set new variables required by lldb/CMakeLists.txt.
    - Build: Runtimes build no longer handles LLVM_LIBDIR_SUFFIX and puts output
    under the 'lib/' directory.  Conditionally use the new path for the
    testing version.
    - Patches: Stop cherry-picking fortify-related revert.  The actual fix
    from upstream will be cherry-picked in a follow-up.
    
    Test: build.py --build-llvm-next
    Change-Id: I8384853ad396a6560217afce891b40b2d77a2d55

--- android_version.py
+++ android_version.py
@@ -22,7 +22,7 @@ _version_read = False
 _patch_level = '1'
 _svn_revision = 'r437112b'
 # svn_revision_next will be newer than the official svn_revision in the future.
-_svn_revision_next = 'r437112b'
+_svn_revision_next = 'r445002'
 
 
 def set_llvm_next(llvm_next: bool):
