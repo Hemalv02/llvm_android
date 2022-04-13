Hemalv02/llvm_android

commit 23b3fc3ca8596ead540657cea5edff08c891682c

Rename PGO profile archive
The new name will allow distinguishing PGO profiles and BOLT
profiles.

Test: presubmit
Bug: 219872481
Change-Id: Ia1072e9d158d7680be5ddd44a91ee21a8aa35e6b

paths.py
@@ -82,7 +82,8 @@ def pgo_profdata_filename() -> str:
 
 
 def pgo_profdata_tarname() -> str:
-    return pgo_profdata_filename() + '.tar.bz2'
+    svn_revision = android_version.get_svn_revision_number()
+    return f'pgo-{svn_revision}.tar.bz2'
 
 
 def pgo_profdata_tar() -> Optional[Path]:

commit 445ca50614a63dfcdc4e62712336924aa1c39916

Fix typo in profile archive name
fixes typo introduced in commit 23b3fc3c

Test: N/A
Change-Id: I36174723b085d8218bc74c8edf91f4d847e48a09

paths.py
@@ -83,7 +83,7 @@ def pgo_profdata_filename() -> str:
 
 def pgo_profdata_tarname() -> str:
     svn_revision = android_version.get_svn_revision_number()
-    return f'pgo-{svn_revision}.tar.bz2'
+    return f'pgo-r{svn_revision}.tar.bz2'
 
 
 def pgo_profdata_tar() -> Optional[Path]: