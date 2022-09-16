411451901c8122b31ce4a404523fb84f197f3692
Yi Kong <yikong@google.com>
2022-09-17 05:50:36 +0800

    Update Kokoro build script to match the official ab build
    But skip tests for now, because it breaks kokoro's artifact collection.
    
    Test: N/A
    Change-Id: If842268f2888385e99ef2f873c821b5666127628

--- kokoro/kokoro_build.sh
+++ kokoro/kokoro_build.sh
@@ -7,5 +7,5 @@ DEST=$top/dest
 python_src=$top/toolchain/llvm_android
 
 DIST_DIR="${DEST}" OUT_DIR="${OUT}" $top/prebuilts/python/linux-x86/bin/python3 \
-$python_src/build.py --pgo --create-tar --build-name "${KOKORO_BUILD_ID}" \
---no-build=windows,lldb
+$python_src/build.py --lto --pgo --bolt --create-tar --build-name "${KOKORO_BUILD_ID}" \
+--no-build=windows --skip-tests
