27a80a520dad4b921c9145a4d2e3c5a26e5bf3bd
Yi Kong <yikong@google.com>
2022-09-17 22:36:27 +0800

    More fixes for the kokoro build
    Some tests in the out directory cannot be opened. Specify the artifact
    regex so that Kokoro wouldn't need to access them.
    
    Do not set DIST_DIR, since Kokoro is able to find the artifact from out.
    
    Test: N/A
    Bug: 246689636
    Change-Id: I0bbda576a7301d370b31d6f27f33911eae31a06f

--- kokoro/kokoro_build.sh
+++ kokoro/kokoro_build.sh
@@ -3,9 +3,8 @@ set -e
 
 top=$(cd $(dirname $0)/../../.. && pwd)
 OUT=$top/out
-DEST=$top/dest
 python_src=$top/toolchain/llvm_android
 
-DIST_DIR="${DEST}" OUT_DIR="${OUT}" $top/prebuilts/python/linux-x86/bin/python3 \
+OUT_DIR="${OUT}" $top/prebuilts/python/linux-x86/bin/python3 \
 $python_src/build.py --lto --pgo --bolt --create-tar --build-name "${KOKORO_BUILD_ID}" \
---no-build=windows --skip-tests
+--no-build=windows
