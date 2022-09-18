5255f15f5dadd738b944383ff2161274bbe81fa9
Yi Kong <yikong@google.com>
2022-09-18 15:17:25 +0800

    Do not run tests on Kokoro for now
    Kokoro still tries to read all artifacts despite we ask it to only
    collect the packaged archive.
    
    Test: N/A
    Change-Id: I6215bd12a4609ae470cbb737f17dd6bfbc2f5c0c

--- kokoro/kokoro_build.sh
+++ kokoro/kokoro_build.sh
@@ -7,4 +7,4 @@ python_src=$top/toolchain/llvm_android
 
 OUT_DIR="${OUT}" $top/prebuilts/python/linux-x86/bin/python3 \
 $python_src/build.py --lto --pgo --bolt --create-tar --build-name "${KOKORO_BUILD_ID}" \
---no-build=windows
+--no-build=windows --skip-tests
