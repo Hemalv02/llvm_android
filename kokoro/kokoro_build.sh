a36916b69e9bc5a76c7a8d9ae52f6b3584cbb2b8
Yi Kong <yikong@google.com>
2022-09-19 12:56:00 +0800

    Clean up out directory on Kokoro
    Test: N/A
    Bug: 246689636
    Change-Id: Ie0790770dfa4396e04f6942843ba92734fd910dc

--- kokoro/kokoro_build.sh
+++ kokoro/kokoro_build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -e
 
-top=$(cd $(dirname $0)/../../.. && pwd)
-OUT=$top/out
-python_src=$top/toolchain/llvm_android
+TOP=$(cd $(dirname $0)/../../.. && pwd)
+OUT=$TOP/out
+DIST=$TOP/dist
+python_src=$TOP/toolchain/llvm_android
 
-OUT_DIR="${OUT}" $top/prebuilts/python/linux-x86/bin/python3 \
+mkdir "${DIST}"
+
+OUT_DIR="${OUT}" DIST_DIR="${DIST}" $TOP/prebuilts/python/linux-x86/bin/python3 \
 $python_src/build.py --lto --pgo --bolt --create-tar --build-name "${KOKORO_BUILD_ID}" \
---no-build=windows --skip-tests
+--no-build=windows
+
+# Kokoro will rsync back everything created by the build. This can take 0
+# minutes for our out directory. Clean up these files if our build was
+# successful.
+rm -rf "${OUT}"
