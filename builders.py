ed502580d634190eae0d0fda4b7c762e086edf9c
Yi Kong <yikong@google.com>
2022-11-08 14:36:26 +0900

    Fix Darwin build
    Darwin hits errors building zstd programs, but there is no need to
    build them anyway.
    
    Bug: 248113754
    Test: build on Darwin
    Change-Id: Ifc86864bd3b70f50fdd8a2fdbb6b1710d37b7fa2

--- builders.py
+++ builders.py
@@ -763,6 +763,12 @@ class ZstdBuilder(base_builders.CMakeBuilder, base_builders.LibInfo):
     with_lib_version: bool = False
 
     @property
+    def cmake_defines(self) -> Dict[str, str]:
+        defines = super().cmake_defines
+        defines['ZSTD_BUILD_PROGRAMS'] = 'OFF'
+        return defines
+
+    @property
     def link_libraries(self) -> List[Path]:
         # LLVM requires both dynamic and static libzstd.
         libs = super().link_libraries
