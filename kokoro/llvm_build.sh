86895d0b0f175b8514ddc8e26e3da806505bed14
Yi Kong <yikong@google.com>
2022-12-07 13:21:41 +0900

    Fix Docker permission errors
    If running the script as root, impersonate as the user that owns the
    source directory.
    
    Test: kokoro presubmit
    Change-Id: Ibda3f3ff85854905f49642fb9c75839f6a50f517

--- kokoro/llvm_build.sh
+++ kokoro/llvm_build.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 set -e
 
-function cleanup {
-  # Kokoro will rsync back everything created by the build. This can take up to 10
-  # minutes for our out directory. Clean up these files at the end.
-  rm -rf "${OUT}"
-}
-
-trap cleanup EXIT
-
 TOP=$(cd $(dirname $0)/../../.. && pwd)
 OUT=$TOP/out
 DIST=$TOP/dist
 python_src=$TOP/toolchain/llvm_android
 
-git config --global --add safe.directory /tmpfs/src/git/external/toolchain-utils
+# Impersonate the user that owns the source directory under Docker.
+if (( $EUID == 0 )); then
+  LOCAL_UID=`stat -c '%u' ${TOP}`
+  LOCAL_GID=`stat -c '%g' ${TOP}`
+  groupadd -g ${LOCAL_GID} build
+  useradd -u ${LOCAL_UID} -g ${LOCAL_GID} -d ${TOP} build
+  su build -c $0 $@
+  exit 0
+fi
 
-mkdir "${DIST}"
+# Kokoro will rsync back everything created by the build. This can take up to 10
+# minutes for our out directory. Clean up these files at the end.
+function cleanup {
+  rm -rf "${OUT}"
+}
+trap cleanup EXIT
 
+mkdir -p "${DIST}"
 if [ $LLVM_BUILD_TYPE == "linux-TOT" ]; then
   OUT_DIR="${OUT}" DIST_DIR="${DIST}" $TOP/prebuilts/python/linux-x86/bin/python3 \
   $python_src/build.py --build-llvm-tot --create-tar --build-name "${KOKORO_BUILD_NUMBER}" \
-  --no-build=windows --skip-tests --sccache
+  --no-build=windows --sccache
 elif [ $LLVM_BUILD_TYPE == "linux-master" ]; then
   OUT_DIR="${OUT}" DIST_DIR="${DIST}" $TOP/prebuilts/python/linux-x86/bin/python3 \
   $python_src/build.py --lto --pgo --bolt --create-tar --build-name "${KOKORO_BUILD_NUMBER}" \
-  --no-build=windows --skip-tests --sccache
+  --no-build=windows --sccache
 elif [ $LLVM_BUILD_TYPE == "darwin-master" ]; then
   OUT_DIR="${OUT}" DIST_DIR="${DIST}" $TOP/prebuilts/python/darwin-x86/bin/python3 \
   $python_src/build.py --lto --pgo --create-tar --build-name "${KOKORO_BUILD_NUMBER}"
