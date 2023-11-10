bda1162232570899d8b9253f0339de3cddb22268
AdityaK <appujee@google.com>
2023-11-10 08:44:55 -0800

    Remove sccache as it is broken
    Bug: b/310206411
    
    Change-Id: I2d70fbd1425d6de0a0672a322e90843f023b7624

--- kokoro/llvm_build.sh
+++ kokoro/llvm_build.sh
@@ -28,19 +28,19 @@ if [ $LLVM_BUILD_TYPE == "linux-TOT" ]; then
   OUT_DIR="${OUT}" DIST_DIR="${DIST}" $TOP/prebuilts/python/linux-x86/bin/python3 \
   $python_src/build.py --build-llvm-next --mlgo --create-tar \
   --build-name "${KOKORO_BUILD_NUMBER}" \
-  --no-build=windows --sccache
+  --no-build=windows
 elif [ $LLVM_BUILD_TYPE == "linux-master" ]; then
   OUT_DIR="${OUT}" DIST_DIR="${DIST}" $TOP/prebuilts/python/linux-x86/bin/python3 \
   $python_src/build.py --lto --pgo --bolt --mlgo --create-tar \
   --build-name "${KOKORO_BUILD_NUMBER}" \
-  --no-build=windows --sccache
+  --no-build=windows
 elif [ $LLVM_BUILD_TYPE == "darwin-master" ]; then
   OUT_DIR="${OUT}" DIST_DIR="${DIST}" $TOP/prebuilts/python/darwin-x86/bin/python3 \
   $python_src/build.py --lto --pgo --create-tar --build-name "${KOKORO_BUILD_NUMBER}"
 elif [ $LLVM_BUILD_TYPE == "windows-master" ]; then
   OUT_DIR="${OUT}" DIST_DIR="${DIST}" $TOP/prebuilts/python/linux-x86/bin/python3 \
   $python_src/build.py --mlgo --create-tar --build-name "${KOKORO_BUILD_NUMBER}" \
-  --no-build=linux --sccache
+  --no-build=linux
 else
   echo "Error: requires LLVM_BUILD_TYPE"
 fi
